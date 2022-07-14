417cac9bc019203126a7247dffe38bf56dbc7360
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-14 20:48:40 +0530

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,183 @@
+'here error handling there are 14 parts'
+"""part-1"""
+# print("ice cream")                             #  ice cream
+# print("venila icecream")                       #  venila icecream
+# try:
+#     print(2/0)
+# except:
+#     print('you have divided by zero')          #  you have divided by zero
+# print("cone icecream")                         #  cone icecream
+# print("cup icecream")                          #   cup icecream
+
+"""part-2"""
+# print("ice cream")                # ice cream
+# print("venila icecream")          # venila ice cream
+# try:
+#     print(8/0)
+# except:
+#     print('not ok divided by zero')   # not ok divided by zero
+# print("cone icecream")                # cone ice cream
+# print("cup icecream")                 # cup ice cream
+
+"""part-3"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# try:
+#     print(b)
+# except Zerodivisionerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-4"""
+# print("ice cream")
+# print("venila icecream")
+# a=2
+# try:
+    # print(a/2)
+    # print(b)
+# exceptzerrordivisionerror:
+    # print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-5"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# try:
+    # print(a/4)
+    # print(a/0)
+# exceptzerodivisionerror:
+    # print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")   
+
+"""part-6"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# try:
+    # print(a/4)
+    # print(b)
+# exceptzerodivisionerror:
+    # print('not ok divided by zero')
+# except nameerror:
+    # print('you have not defined the name')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-7"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a)
+#     print(a/0)
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-8"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a/0)
+#     print(a+"hi")
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-9"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a/0)
+#     print(a+a)
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-10"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a)
+#     print(b+"hi")
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-11"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a)
+#     print(,)
+# exceptsyntaxerror:
+#     print('not ok divided by zero')
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-12"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# finally:
+#     print("its ok next time don't divided by zero")
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-13"""
+# print("ice cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a/0)
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# finally:
+#     print("its ok next time don't divided by zero")
+# print("cone icecream")
+# print("cup icecream")
+
+"""part-14"""
+#  cream")
+# print("venila icecream")
+# a=4
+# b='5'
+# try:
+#     print(a/4)
+#     print(a/10)
+# exceptzerodivisionerror:
+#     print('not ok divided by zero')
+# else:
+#     print("its ok next time don't divided by zero")
+# print("cone icecream")
+# print("cup icecream")
